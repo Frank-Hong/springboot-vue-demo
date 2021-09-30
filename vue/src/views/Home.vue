945ccf1343e3a37bf0ab4cc4760c19b01f3e8eb6
Frank-Hong <3415513441@qq.com>
2021-10-01 00:56:04 +0800

    增删改查

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,44 +1,57 @@
 <template>
   <div style="padding: 10px">
+<!--    功能区域-->
     <div style="margin: 10px 0">
-      <el-button type="primary">新增</el-button>
+      <el-button type="primary" @click="add">新增</el-button>
       <el-button type="primary">导入</el-button>
       <el-button type="primary">导出</el-button>
     </div>
+<!--    搜索区域-->
     <div style="margin:10px 0">
       <el-input v-model="search" placeholder="请输入关键字" style="width: 20%"></el-input>
-      <el-button type="primary" style="margin-left: 5px">查询</el-button>
+      <el-button type="primary" style="margin-left: 5px" @click="">查询</el-button>
     </div>
+<!--    显示查询数据-->
     <el-table
         :data="tableData"
         border
         stripe
         style="width: 100%">
       <el-table-column
-          prop="date"
-          label="Date"
+          prop="id"
+          label="ID"
           sortable
       >
       </el-table-column>
       <el-table-column
-          prop="name"
-          label="Name">
+          prop="username"
+          label="用户名">
+      </el-table-column>
+      <el-table-column
+          prop="nickName"
+          label="昵称">
+      </el-table-column>
+      <el-table-column
+          prop="age"
+          label="年龄">
+      </el-table-column>
+      <el-table-column
+          prop="sex"
+          label="性别">
       </el-table-column>
       <el-table-column
           prop="address"
-          label="Address">
+          label="地址">
       </el-table-column>
       <el-table-column
           fixed="right"
           label="操作"
       >
         <template #default="scope">
-          <el-button size="min" @click="handleEdit(scope.$index,scope.row)" >编辑</el-button>
-          <el-popconfirm
-              title="确认删除吗？"
-          >
+          <el-button size="min" @click="handleEdit(scope.row)" >编辑</el-button>
+          <el-popconfirm title="确认删除吗？" @confirm="handleDelete(scope.row.id)">
             <template #reference>
-              <el-button size="mini" @click="handleDelete(scope.$index,scope.row)" type="danger">删除</el-button>
+              <el-button size="mini" type="danger">删除</el-button>
             </template>
           </el-popconfirm>
         </template>
@@ -48,18 +61,50 @@
       <el-pagination
           @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
-          :current-page="currentPage4"
+          :current-page="currentPage"
           :page-sizes="[5, 10, 20]"
-          :page-size="10"
+          :page-size="pageSize"
           layout="total, sizes, prev, pager, next, jumper"
           :total="total">
       </el-pagination>
+
+      <el-dialog title="提示" v-model="dialogVisible" width="30%">
+        <el-form :model="form" label-width="120px">
+          <el-form-item label="用户名">
+            <el-input v-model="form.username" style="width: 80%"></el-input>
+          </el-form-item>
+          <el-form-item label="昵称">
+            <el-input v-model="form.nickName" style="width: 80%"></el-input>
+          </el-form-item>
+          <el-form-item label="年龄">
+            <el-input v-model="form.age" style="width: 80%"></el-input>
+          </el-form-item>
+          <el-form-item label="性别">
+            <el-radio v-model="form.sex" label="男"></el-radio>
+            <el-radio v-model="form.sex" label="女"></el-radio>
+            <el-radio v-model="form.sex" label="未知"></el-radio>
+          </el-form-item>
+          <el-form-item label="地址">
+            <el-input type="textarea" v-model="form.address" style="width: 80%"></el-input>
+          </el-form-item>
+        </el-form>
+
+        <template #footer>
+          <span class="dialog-footer">
+            <el-button @click="dialogVisible=false"> 取消</el-button>
+            <el-button type="primary" @click="save">确定</el-button>
+          </span>
+        </template>
+      </el-dialog>
+
     </div>
   </div>
 </template>
 
 <script>
 
+
+import request from "../../utils/request";
 
 export default {
   name: 'Home',
@@ -68,42 +113,100 @@ export default {
   },
   data(){
     return {
+      form:{},
+      dialogVisible:false,
       search:'',
+      pageSize:10,
       currentPage:1,
       total:10,
       tableData:[
-        {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }
+
       ]
     }
   },
+  created() {
+    this.load()
+  },
   methods:{
-    handleEdit(){
-
+    load(){
+      request.get("api/user",{
+      params:{
+        pageNum:this.currentPage,
+        pageSize:this.pageSize,
+        search:this.search
+      }
+      }).then(res=>{
+        this.tableData=res.data.records
+        this.total=res.data.total
+      })
     },
-    handleDelete(){
-
+    add(){
+      this.dialogVisible=true
+      this.form={}
     },
-    handleSizeChange(){
-
+    save(){
+      if(this.form.id){
+        request.put("/api/user",this.form).then(res=>{
+          console.log(res)
+          if(res.code==='0'){
+            this.$message({
+              type:"success",
+              message:"更新成功"
+            })
+          } else{
+            this.$message({
+              type:"error",
+              message:"更新失败"
+            })
+          }
+        })
+      }
+      else {
+        request.post("/api/user", this.form).then(res => {
+          console.log(res)
+          if(res.code==='0'){
+            this.$message({
+              type:"success",
+              message:"新增成功"
+            })
+          } else{
+            this.$message({
+              type:"error",
+              message:"新增失败"
+            })
+          }
+        })
+      }
+      this.load()
+      this.dialogVisible=false
     },
-    handleCurrentChange(){
-
+    handleEdit(row){
+      this.form=JSON.parse(JSON.stringify(row))
+      this.dialogVisible=true
+    },
+    handleDelete(id){
+      request.delete("/api/user/"+id).then(res=>{
+        if(res.code==='0'){
+          this.$message({
+            type:"success",
+            message:"删除成功"
+          })
+        } else{
+          this.$message({
+            type:"error",
+            message:"删除失败"
+          })
+        }
+      })
+      this.load()
+    },
+    handleSizeChange(pageSize){//改变每页个数触发
+      this.pageSize=pageSize
+      this.load()
+    },
+    handleCurrentChange(pageNum){//改变当前页码触发
+      this.currentPage=pageNum
+      this.load()
     }
   }
 }
