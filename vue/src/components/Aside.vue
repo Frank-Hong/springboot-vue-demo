331fd388fb36f62fb484bfd2eb77e6165b8a56c4
Frank-Hong <3415513441@qq.com>
2021-10-02 14:36:25 +0800

    设置菜单高亮

--- vue/src/components/Aside.vue
+++ vue/src/components/Aside.vue
@@ -3,21 +3,28 @@
   <div>
     <el-menu
         style="width: 200px;min-height: calc(100vh - 50px)"
-        default-active="2"
+        :default-active="path"
         router
         class="el-menu-vertical-demo">
       <el-sub-menu index="1">
         <template #title>系统管理</template>
-        <el-menu-item index="user">用户管理</el-menu-item>
+        <el-menu-item index="/user">用户管理</el-menu-item>
       </el-sub-menu>
-      <el-menu-item index="book">书籍管理</el-menu-item>
+      <el-menu-item index="/book">书籍管理</el-menu-item>
     </el-menu>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Aside"
+  name: "Aside",
+  data(){
+    return{
+      path:this.$route.path
+    }
+  },
+  created() {
+  }
 }
 </script>
 
--- vue/src/views/Book.vue
+++ vue/src/views/Book.vue
@@ -1,13 +1,204 @@
 <template>
+  <div style="padding: 10px">
+    <!--    功能区域-->
+    <div style="margin: 10px 0">
+      <el-button type="primary" @click="add">新增</el-button>
+      <el-button type="primary">导入</el-button>
+      <el-button type="primary">导出</el-button>
+    </div>
+    <!--    搜索区域-->
+    <div style="margin:10px 0">
+      <el-input v-model="search" placeholder="请输入关键字" style="width: 20%"></el-input>
+      <el-button type="primary" style="margin-left: 5px" @click="">查询</el-button>
+    </div>
+    <!--    显示查询数据-->
+    <el-table
+        :data="tableData"
+        border
+        stripe
+        style="width: 100%">
+      <el-table-column
+          prop="id"
+          label="ID"
+          sortable
+      >
+      </el-table-column>
+      <el-table-column
+          prop="name"
+          label="名称">
+      </el-table-column>
+      <el-table-column
+          prop="price"
+          label="单价">
+      </el-table-column>
+      <el-table-column
+          prop="author"
+          label="作者">
+      </el-table-column>
+      <el-table-column
+          prop="createTime"
+          label="出版时间">
+      </el-table-column>
+      <el-table-column
+          fixed="right"
+          label="操作"
+      >
+        <template #default="scope">
+          <el-button size="min" @click="handleEdit(scope.row)" >编辑</el-button>
+          <el-popconfirm title="确认删除吗？" @confirm="handleDelete(scope.row.id)">
+            <template #reference>
+              <el-button size="mini" type="danger">删除</el-button>
+            </template>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div style="margin: 10px 0">
+      <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage"
+          :page-sizes="[5, 10, 20]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total">
+      </el-pagination>
 
+      <el-dialog title="提示" v-model="dialogVisible" width="30%">
+        <el-form :model="form" label-width="120px">
+          <el-form-item label="名称">
+            <el-input v-model="form.name" style="width: 80%"></el-input>
+          </el-form-item>
+          <el-form-item label="单价">
+            <el-input v-model="form.price" style="width: 80%"></el-input>
+          </el-form-item>
+          <el-form-item label="作者">
+            <el-input v-model="form.author" style="width: 80%"></el-input>
+          </el-form-item>
+          <el-form-item label="出版时间">
+            <el-date-picker v-model="form.createTime" value-format="YYYY-MM-DD" type="date" style="width: 80%" clearable></el-date-picker>
+          </el-form-item>
+        </el-form>
+
+        <template #footer>
+          <span class="dialog-footer">
+            <el-button @click="dialogVisible=false"> 取消</el-button>
+            <el-button type="primary" @click="save">确定</el-button>
+          </span>
+        </template>
+      </el-dialog>
+
+    </div>
+  </div>
 </template>
 
 <script>
+
+
+import request from "../../utils/request";
+
 export default {
-  name: "Book"
+  name: 'Book',
+  components: {
+
+  },
+  data(){
+    return {
+      form:{},
+      dialogVisible:false,
+      search:'',
+      pageSize:10,
+      currentPage:1,
+      total:10,
+      tableData:[
+
+      ]
+    }
+  },
+  created() {
+    this.load()
+  },
+  methods:{
+    load(){
+      request.get("/book",{
+        params:{
+          pageNum:this.currentPage,
+          pageSize:this.pageSize,
+          search:this.search
+        }
+      }).then(res=>{
+        this.tableData=res.data.records
+        this.total=res.data.total
+      })
+    },
+    add(){
+      this.dialogVisible=true
+      this.form={}
+    },
+    save(){
+      if(this.form.id){
+        request.put("/book",this.form).then(res=>{
+          console.log(res)
+          if(res.code==='0'){
+            this.$message({
+              type:"success",
+              message:"更新成功"
+            })
+          } else{
+            this.$message({
+              type:"error",
+              message:"更新失败"
+            })
+          }
+        })
+      }
+      else {
+        request.post("/book", this.form).then(res => {
+          console.log(res)
+          if(res.code==='0'){
+            this.$message({
+              type:"success",
+              message:"新增成功"
+            })
+          } else{
+            this.$message({
+              type:"error",
+              message:"新增失败"
+            })
+          }
+        })
+      }
+      this.load()
+      this.dialogVisible=false
+    },
+    handleEdit(row){
+      this.form=JSON.parse(JSON.stringify(row))
+      this.dialogVisible=true
+    },
+    handleDelete(id){
+      request.delete("/book/"+id).then(res=>{
+        if(res.code==='0'){
+          this.$message({
+            type:"success",
+            message:"删除成功"
+          })
+        } else{
+          this.$message({
+            type:"error",
+            message:"删除失败"
+          })
+        }
+      })
+      this.load()
+    },
+    handleSizeChange(pageSize){//改变每页个数触发
+      this.pageSize=pageSize
+      this.load()
+    },
+    handleCurrentChange(pageNum){//改变当前页码触发
+      this.currentPage=pageNum
+      this.load()
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
